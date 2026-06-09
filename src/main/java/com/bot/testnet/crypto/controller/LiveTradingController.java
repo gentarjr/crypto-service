682306ab041f87ad481ffc822f23aebf99d6e7dd
gentar <sayagentar@gmail.com>
2026-06-10 04:02:42 +0700

    fixing

--- src/main/java/com/bot/testnet/crypto/controller/LiveTradingController.java
+++ src/main/java/com/bot/testnet/crypto/controller/LiveTradingController.java
@@ -7,10 +7,7 @@ import com.bot.testnet.crypto.service.trading.OrderExecutorService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -91,5 +88,11 @@ public class LiveTradingController {
         stats.put("wins", tradeHistoryRepository.countWinsSince(startOfDay));
         stats.put("todayPnl", tradeHistoryRepository.sumPnlSince(startOfDay));
         return stats;
+    }
+
+    @DeleteMapping("/history/reset")
+    public Map<String, String> resetHistory() {
+        tradeHistoryRepository.deleteAll();
+        return Map.of("status", "OK", "message", "Trade history cleared");
     }
 }
